ee1823cb187cfde69ca679429effc27591f649af
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-01-02 12:03:39 -0800

    made tests pass up until objects - line 17 in fixtures.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,51 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
+//NEED TO USE RECURSION
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var finalString = '';
+
+	var toStringString = function(opening, closing, array) {
+		array.forEach(function(element) {
+			opening += element;
+		})
+		return opening + closing;
+	}
+
+	if (typeof obj === 'string') {
+		var split = obj.split('');
+		return toStringString('"', '"', split);
+	}
+	if (Array.isArray(obj)) {
+		var openingBracket = '[';
+		var closingBracket = ']';
+
+		if (obj.length === 0) {
+			return '[]';
+		} else if (obj.length === 1) {
+			obj.forEach(function(element) {
+				if (typeof element === 'number') {
+					openingBracket += element;
+				} else {
+					openingBracket += stringifyJSON(element);
+				}
+			})
+			return openingBracket + closingBracket;
+		} else {
+			obj.forEach(function(element) {
+				if (typeof element === 'number') {
+					openingBracket += element + ',';
+				} else {
+					openingBracket += stringifyJSON(element) + ',';
+				}
+			})
+			var result = openingBracket.substring(0, openingBracket.length - 1) + closingBracket;
+			return result;
+		}
+	} else {
+		//Work on Objects
+		//Plan to eventually create helper functions to work for both arrays and objects
+	}
+	finalString += obj;
+	return finalString;
 };
